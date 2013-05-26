bf5b4344d87c9eba57e2dda41c479f1fca293b8d
Aaron Springut <ajbear>
2013-05-25 20:06:28 -0500

    added edit column to recipes

--- recipes/tables.py
+++ recipes/tables.py
@@ -7,8 +7,12 @@ class RecipeTable(tables.Table):
     #custom column with template for link because linkcolumn does not work
     name_link ='<a href = "../view_recipe?recipe={{record.pk}}">{{record.name}}</a>'
     recipe_name = tables.TemplateColumn(name_link, order_by=A("name"))
+    #custom column for editing
+    edit_link ='<a href = "../add_recipe?recipe{{record.pk}}">edit'
+    edit_recipe = tables.TemplateColumn(edit_link, orderable=False)
+
     class Meta:
         model = Recipe
         attrs = {"class": "table table-stripped"}
-        exclude = ("id", "restaurant", 'name')
-        sequence = ("recipe_name",)
+        exclude = ("id", "restaurant", 'name', 'instruction')
+        sequence = ("recipe_name","cost","edit_recipe","delete")
