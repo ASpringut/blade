c48149b36114f4735e4067100ac24674c648f140
Aaron <aaron@FLANKER.(none)>
2013-06-02 20:25:16 -0500

    changed settings for aws"

--- Blade/settings.py
+++ Blade/settings.py
@@ -26,6 +26,14 @@ DATABASES = {
     }
 }
 
+if not DEBUG:
+    AWS_STORAGE_BUCKET_NAME = os.environ['AWS_STORAGE_BUCKET_NAME']
+    AWS_ACCESS_KEY_ID = os.environ['AWS_ACCESS_KEY_ID']
+    AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
+    STATICFILES_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
+    S3_URL = 'http://%s.s3.amazonaws.com/' % AWS_STORAGE_BUCKET_NAME
+    STATIC_URL = S3_URL
+
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
 # although not all choices may be available on all operating systems.
@@ -133,7 +141,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'inventory',
-	'Measures',
+    'Measures',
     'recipes',
     'service',
     # Uncomment the next line to enable the admin:
@@ -141,6 +149,7 @@ INSTALLED_APPS = (
     # Uncomment the next line to enable admin documentation:
     'django.contrib.admindocs',
     'django_tables2',
+    'storages',
 )
 
 # A sample logging configuration. The only tangible logging
